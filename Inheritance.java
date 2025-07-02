dfd6d5fd0a99af4b7dac42a3204684d8c987143b
Ajay Raj Tomar <ajayrajtomar.143@gmail.com>
2025-07-02 14:34:22 +0530

    Added about

---
+++ Inheritance.java
@@ -0,0 +1,33 @@
+// class Animal{
+//     String type;
+//     public void tail(String type)
+//     {
+//         this.type = type;
+//         System.out.println(type+" has a tail");
+//     }
+// }
+// class Dog extends Animal
+// {
+//     public void bark()
+//     {
+//         System.out.println("Bhow Bhow");
+//     }
+// }
+// class Cat extends Animal
+// {
+//     public void mew()
+//     {
+//         System.out.println("Mew Mew");
+//     }
+// }
+// public class Inheritance {
+//     public static void main(String[] args) {
+//         Dog d1 = new Dog();
+//         d1.bark();
+//         d1.tail("DOG");
+//         Cat c1 = new Cat();
+//         c1.mew();
+//         c1.tail("Cat");
+//     }
+    
+// }
